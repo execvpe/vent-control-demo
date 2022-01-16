3285da0e4272317ace2e2220fc5bcf061b0c8114
Martin Weinzierl <152305632+execvpe@users.noreply.github.com>
2022-01-16 03:05:01 +0100

    Implement 24h average deviation check
    Buzz if the actual value deviates more than a constant value
    from the average.

--- src/cpp/main.cpp
+++ src/cpp/main.cpp
@@ -10,6 +10,15 @@
 #include "request.h"	   // requestApi()
 #include "stringMacros.h"  // STRING_STARTS_WITH()
 
+#define MAX_DEVIATION_FROM_AVERAGE_KELVIN (4.0F)
+#define MIN_TEMPERATURE_CELSIUS 20
+
+#define SLEEP_AFTER_FAILURE_SEC 5
+#define SLEEP_AFTER_SUCCESS_SEC 15
+
+#define AVERAGE_MAX_VALUES ((60 / SLEEP_AFTER_SUCCESS_SEC) * 60 * 24)
+// 4 readings/second * 60 seconds/minute * 24 minutes/day
+
 namespace {
 	template <typename T>
 	T vectorAverage(std::vector<T> const &vec) {
@@ -19,6 +28,25 @@ namespace {
 
 		return std::reduce(vec.begin(), vec.end()) /
 			   static_cast<T>(vec.size());
+	}
+
+	bool buzz(requestInfo_t *reqInfo) {
+		char *request;
+		if (requestApi(reqInfo, &request) == 0) {
+			std::cerr << "Buzzing request failed! - " << std::strerror(errno) << std::endl;
+			errno = 0;
+			return false;
+		}
+
+		if (STRING_STARTS_WITH(request, "OK.")) {
+			std::cout << "Buzzing successful!" << std::endl;
+			std::free(request);
+			return true;
+		}
+
+		std::cerr << "Buzzing failed!" << std::endl;
+		std::free(request);
+		return false;
 	}
 }  // namespace
 
@@ -42,7 +70,7 @@ int main(int argc, char **argv) {
 		if (requestApi(&reqCelsius, &request) == 0) {
 			std::cerr << "Temperature request failed! - " << std::strerror(errno) << std::endl;
 			errno = 0;
-			sleep(5);
+			sleep(SLEEP_AFTER_FAILURE_SEC);
 			continue;
 		}
 
@@ -59,35 +87,45 @@ int main(int argc, char **argv) {
 
 		std::free(request);
 
-		if (floatStorage.size() >= 4) {
+		if (floatStorage.size() >= AVERAGE_MAX_VALUES) {
 			// Remove oldest element from the beginning of the vector
 			floatStorage.erase(floatStorage.begin());
 		}
 
 		floatStorage.push_back(value);	// Add new element at the end of the vector
 
-		float average = vectorAverage(floatStorage);
-		std::cout << "Average(" << floatStorage.size() << "): " << average << " C" << std::endl;
+		// Buzz if the value is below MIN_TEMPERATURE_CELSIUS
+		if (value < MIN_TEMPERATURE_CELSIUS) {
+			std::cout << "Actual value dropped below "
+					  << MIN_TEMPERATURE_CELSIUS << " C!" << std::endl;
 
-		if (average < 20) {
-			if (requestApi(&reqBuzzer, &request) == 0) {
-				std::cerr << "Buzzing request failed! - " << std::strerror(errno) << std::endl;
-				errno = 0;
-				sleep(5);
+			if (!buzz(&reqBuzzer)) {
+				sleep(SLEEP_AFTER_FAILURE_SEC);
 				continue;
 			}
 
-			if (STRING_STARTS_WITH(request, "OK.")) {
-				std::cout << "Buzzing successful!" << std::endl;
-			} else {
-				std::cerr << "Buzzing failed!" << std::endl;
-			}
-
-			std::free(request);
+			std::cout << "Sleeping for " << SLEEP_AFTER_SUCCESS_SEC << " seconds..." << std::endl;
+			sleep(SLEEP_AFTER_SUCCESS_SEC);
+			continue;
 		}
 
-		std::cout << "Sleeping for 15 seconds..." << std::endl;
-		sleep(15);
+		float average = vectorAverage(floatStorage);
+		std::cout << "Average(values: " << floatStorage.size() << "): " << average << " C" << std::endl;
+
+		// Buzz if the value deviates more than MAX_DEVIATION_FROM_AVERAGE_KELVIN from the average
+		// No std::abs() because it does not matter if it gets warmer at some point
+		if ((average - value) > MAX_DEVIATION_FROM_AVERAGE_KELVIN) {
+			std::cout << "Actual value deviated more than "
+					  << MAX_DEVIATION_FROM_AVERAGE_KELVIN << " K from the average!" << std::endl;
+
+			if (!buzz(&reqBuzzer)) {
+				sleep(SLEEP_AFTER_FAILURE_SEC);
+				continue;
+			}
+		}
+
+		std::cout << "Sleeping for " << SLEEP_AFTER_SUCCESS_SEC << " seconds..." << std::endl;
+		sleep(SLEEP_AFTER_SUCCESS_SEC);
 	}
 
 	return EXIT_SUCCESS;
